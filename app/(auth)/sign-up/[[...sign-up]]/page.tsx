ac39bcbea67ea22b72642d6fcd695a9bd08a714c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-01-16 08:21:30 +0530

    Modifyu the sign-up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white shadow-lg rounded-lg">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
